a2cc6721fbb1eab445255511626bcbccfb26fc3e
OussamaMaroufi <oussama.maaroufi.ing@gmail.com>
2023-01-25 14:21:18 +0100

    initial commit

---
+++ src/main/java/com/example/learnspringbootsecurity/LearnSpringbootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnspringbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringbootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learnspringbootsecurity/LearnSpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnspringbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
